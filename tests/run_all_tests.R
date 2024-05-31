a7c135f843d8248960b0031c238b0c5aaee5b59a
Mani Tahami <Mani81t@gmail.com>
2024-05-31 12:26:03 -0400

    fixed the typo in the file name

---
+++ tests/run_all_tests.R
@@ -0,0 +1,29 @@
+library(testthat)
+
+source("tests/common/common_functions.R")
+
+# Function to run tests in a directory
+run_tests_in_directory <- function(directory) {
+  # List all test files in the directory
+  path <- paste("tests/", directory, sep = "")
+  test_files <- list.files(path=path, pattern = "_tests\\.R$", full.names = TRUE)
+  
+  # Execute each test file
+  for (file in test_files) {
+    test_file(file)
+    
+  }
+}
+
+# Get all the folders inside tests folder
+
+test_directories <- list.dirs("tests", full.names = FALSE, recursive = FALSE)
+
+
+# Run tests in each directory
+for (dir in test_directories) {
+  cat(paste("Running tests in directory:", dir, "\n"))
+  run_tests_in_directory(dir)
+}
+
+
