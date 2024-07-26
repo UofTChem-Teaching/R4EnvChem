4a64fba5066589ffd6324e79bc165d40b61be518
Mani Tahami <Mani81t@gmail.com>
2024-07-26 12:09:07 -0400

    Improve error messages in exercise test files (excluding Rmd files)

--- exercises/chapter 20/chapter20_tests.R
+++ exercises/chapter 20/chapter20_tests.R
@@ -1,15 +1,37 @@
 # Unit tests for chapter 20 exercises
-# Load  packages
+
+# Load packages
 library(testthat)
 library(here)
 library(fs)
+library(withr)
+library(knitr)
+library(tidyverse)
 
-if (file.exists("exercises/helper_functions.R")) {
-  source("exercises/helper_functions.R")
+# Define the function to run all chunks from Rmd file
+run_all_chunks <- function(rmd, envir = globalenv()) {
+  tempR <- file.path("temp.R")  # Create temporary file in the same directory
+  on.exit(unlink(tempR))
+  
+  purl(input = rmd, output = tempR)
+  
+  # Source the temporary R script with chdir = TRUE so that the working directory is set to the Rmd file's directory
+  source(tempR, chdir = TRUE, local = envir)
+  
 }
 
-# Specify the path to your Rmd file using here::here()
-rmd_file_path <- here("exercises", "chapter 20", "chapter20.Rmd")
+# Function to determine the correct path for the Rmd file
+get_rmd_path <- function(folder_name,filename) {
+  paths <- list(here(filename), here("exercises", folder_name, filename))
+  for (path in paths) {
+    if (file.exists(path))
+      return(path)
+  }
+  stop("Rmd file not found!")
+}
+
+# Specify the path to your Rmd file
+rmd_file_path <- get_rmd_path("chapter 20","chapter20.Rmd")
 
 # Exercise 2: Building Multiple Linear Regression Models
 test_that("Linear Model Test", {
@@ -17,13 +39,40 @@ test_that("Linear Model Test", {
   # copy contents of current directory to temp directory
   dir_copy(dirname(rmd_file_path), tmp_dir, overwrite = TRUE)
   
-  withr::with_dir(tempdir(), {
+  with_dir(tempdir(), {
     # # Run all chunks in the Rmd file to execute the exercises
     suppressWarnings(run_all_chunks(rmd_file_path))
-    expect_true("TBEP" %in% models$chemical)
-    expect_true(nrow(models) == 3)
-    expect_true(models$intercept[1] > 5000)
-    expect_true(models$slope[1] > 50000)
+    
+    # Custom error messages for required checks
+    custom_error_message <- case_when(
+      !"TBEP" %in% models$chemical ~
+        sprintf(
+          "The 'models' dataframe does not contain the chemical 'TBEP'. Existing chemicals: %s",
+          paste(models$chemical, collapse = ", ")
+        ),
+      nrow(models) != 3 ~
+        sprintf(
+          "The 'models' dataframe does not have 3 rows. It has %d rows.",
+          nrow(models)
+        ),
+      models$intercept[1] <= 5000 ~
+        sprintf(
+          "The intercept for the first model is not greater than 5000. It is %f.",
+          models$intercept[1]
+        ),
+      models$slope[1] <= 50000 ~
+        sprintf(
+          "The slope for the first model is not greater than 50000. It is %f.",
+          models$slope[1]
+        ),
+      TRUE ~ NA_character_
+    )
+    
+    # Check for required conditions in the models dataframe
+    expect_true("TBEP" %in% models$chemical, info = custom_error_message)
+    expect_true(nrow(models) == 3, info = custom_error_message)
+    expect_true(models$intercept[1] > 5000, info = custom_error_message)
+    expect_true(models$slope[1] > 50000, info = custom_error_message)
   })
   
   # remove the temp directory
@@ -36,13 +85,28 @@ test_that("Prediction Test", {
   # copy contents of current directory to temp directory
   dir_copy(dirname(rmd_file_path), tmp_dir, overwrite = TRUE)
   
-  withr::with_dir(tempdir(), {
+  with_dir(tempdir(), {
     # # Run all chunks in the Rmd file to execute the exercises
     suppressWarnings(run_all_chunks(rmd_file_path))
-    expect_true(nrow(sample_data) == 33)
-    expect_true(!any(is.na(sample_data)))
+    
+    # Custom error messages for required checks
+    custom_error_message <- case_when(
+      nrow(sample_data) != 33 ~
+        sprintf(
+          "The 'sample_data' dataframe does not have 33 rows. It has %d rows.",
+          nrow(sample_data)
+        ),
+      any(is.na(sample_data)) ~
+        "The 'sample_data' dataframe contains NA values.",
+      TRUE ~ NA_character_
+    )
+    
+    # Check for required conditions in the sample_data dataframe
+    expect_true(nrow(sample_data) == 33, info = custom_error_message)
+    expect_true(!any(is.na(sample_data)), info = custom_error_message)
   })
   
   # remove the temp directory
   unlink(tmp_dir, recursive = TRUE)
 })
+
